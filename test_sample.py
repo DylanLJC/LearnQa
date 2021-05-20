af4ebead8fdf19210c480dbfa1e5cc43377a5447
LJC <571134227@qq.com>
2021-05-21 00:29:09 +0800

    pytest测试2

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,29 @@
+import pytest
+
+
 def inc(x):
     return x + 1
 
 
 def test_answer():
     assert inc(3) == 5
+
+
+def test_answer1():
+    assert inc(4) == 5
+
+
+class TestDemo:
+    # def __init__(self):   不能有init方法
+    def test_a(self):
+        print("a")
+
+    def test_b(self):
+        print("b")
+
+    def test_c(self):
+        print("c")
+
+
+if __name__ == '__main__':
+    pytest.main(['test_sample.py :: TestDemo'])
